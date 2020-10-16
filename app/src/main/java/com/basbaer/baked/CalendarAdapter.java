10b0944c506a8506b9d5138a32009d355acaefe3
discobreak21 <bastianbaer@hotmail.com>
2020-10-16 08:47:36 +0200

    change onClick Calendar

--- app/src/main/java/com/basbaer/baked/CalendarAdapter.java
+++ app/src/main/java/com/basbaer/baked/CalendarAdapter.java
@@ -27,6 +27,8 @@ class CalendarAdapter extends BaseAdapter {
     private final Context context;
     private TextView datesTextView;
     public static int weeksOfMonth;
+    private static ArrayList<TrackedActivity> activitiesOfTheDay;
+    private static View currentConvertView;
 
 
     public CalendarAdapter(Context context, HashMap<Integer, Calendar> datesAL) {
@@ -59,10 +61,16 @@ class CalendarAdapter extends BaseAdapter {
 
 
         //inflates the View with the custom_calendar_day.xml
-        convertView = LayoutInflater.from(context).inflate(R.layout.custom_calendar_day, parent, false);
+        if(convertView == null) {
+            currentConvertView = LayoutInflater.from(context).inflate(R.layout.custom_calendar_day, parent, false);
 
-        LinearLayout daysCl = convertView.findViewById(R.id.daysCL);
-        datesTextView = convertView.findViewById(R.id.daysTV);
+            convertView = currentConvertView;
+        }else{
+            currentConvertView = convertView;
+        }
+
+        LinearLayout daysCl = currentConvertView.findViewById(R.id.daysCL);
+        datesTextView = currentConvertView.findViewById(R.id.daysTV);
 
         //the date that is currently created
         Calendar displayedDate = datesHashMap.get(position);
@@ -77,7 +85,7 @@ class CalendarAdapter extends BaseAdapter {
             datesTextView.setTextColor(Color.parseColor("#222222"));
             datesTextView.setTextAppearance(R.style.TextAppearance_AppCompat_Body1);
             datesTextView.setTextSize(18f);
-            //datesTextView.setPadding(5,5,5,0);
+
 
 
         } else {
@@ -86,90 +94,70 @@ class CalendarAdapter extends BaseAdapter {
 
             datesTextView.setText(String.valueOf(displayedDate.get(Calendar.DAY_OF_MONTH)));
 
-            ArrayList<TrackedActivity> activitiesOfTheDay = TrackedActivity.getActivities(displayedDate);
+            activitiesOfTheDay = TrackedActivity.getActivities(displayedDate);
+
+            if(!activitiesOfTheDay.isEmpty()){
+                //there is definetly min 1 entry
+                setUpFirstEntryOfTheDay();
+
+                if(activitiesOfTheDay.size() == 1) {
+
+                    //intent to the AddActivity (if there is more than one entry, first a dayOverview opens)
+                    onClickListenerForFirstActivity(context);
+
+                }else if(activitiesOfTheDay.size() > 1){
 
 
-            //setting the entries of the day
-            if (activitiesOfTheDay.size() == 1) {
+                    setUpSecondEntryOfTheDay();
 
-                setUpFirstEntryOfTheDay(convertView, activitiesOfTheDay);
+                    if(activitiesOfTheDay.size() > 2){
 
-            } else if (activitiesOfTheDay.size() == 2) {
+                        setUpThirdEntryOfTheDay();
 
-                setUpFirstEntryOfTheDay(convertView, activitiesOfTheDay);
+                        if(activitiesOfTheDay.size() == 4){
 
-                setUpSecondEntryOfTheDay(convertView, activitiesOfTheDay);
+                            setUpForthEntryOfTheDay();
 
+                        }else if(activitiesOfTheDay.size() > 4){
 
-            } else if (activitiesOfTheDay.size() == 3) {
+                            setUpMoreThanFourEntryOfTheDay();
 
-                setUpFirstEntryOfTheDay(convertView, activitiesOfTheDay);
+                        }
 
-                setUpSecondEntryOfTheDay(convertView, activitiesOfTheDay);
+                    }
 
-                setUpThirdEntryOfTheDay(convertView, activitiesOfTheDay);
+                }
 
-            } else if (activitiesOfTheDay.size() == 4) {
-
-                setUpFirstEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpSecondEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpThirdEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpForthEntryOfTheDay(convertView, activitiesOfTheDay);
-
-            } else if (activitiesOfTheDay.size() > 4) {
-
-                setUpFirstEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpSecondEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpThirdEntryOfTheDay(convertView, activitiesOfTheDay);
-
-                setUpMoreThanFourEntryOfTheDay(convertView);
 
             }
+
+
+
 
             daysCl.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
 
-                    Intent intentToAddActivity = new Intent(context, AddActivity.class);
+                    if(activitiesOfTheDay.size() < 2) {
 
-                    long dateInSeconds = datesHashMap.get(position).getTime().getTime();
+                        Intent intentToAddActivity = new Intent(context, AddActivity.class);
 
-                    intentToAddActivity.putExtra("date", dateInSeconds);
+                        long dateInSeconds = datesHashMap.get(position).getTime().getTime();
 
-                    context.startActivity(intentToAddActivity);
+                        intentToAddActivity.putExtra("date", dateInSeconds);
+
+                        context.startActivity(intentToAddActivity);
+
+                    }else{
+
+                        //intent to day overview
+
+                    }
                 }
             });
 
 
-            //intent to the AddActivity
-            if (!activitiesOfTheDay.isEmpty()) {
-                TextView textView_entry_1 = convertView.findViewById(R.id.ActivityTV_1);
 
-                final String firstActivity = activitiesOfTheDay.get(0).getActivityName();
-
-                textView_entry_1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        Intent intentToActivityOverview = new Intent(context, ActivityOverview.class);
-
-
-                        intentToActivityOverview.putExtra("activity", firstActivity);
-
-
-                        context.startActivity(intentToActivityOverview);
-
-
-
-                    }
-                });
-
-            }
 
 
 
@@ -227,78 +215,104 @@ class CalendarAdapter extends BaseAdapter {
         return height / (weeksOfMonth + 1);
     }
 
-    private static void setUpFirstEntryOfTheDay(View convertView, ArrayList<TrackedActivity> activitiesOfTheDay) {
+    private static void setUpFirstEntryOfTheDay() {
 
 
-        CardView cardView_1 = convertView.findViewById(R.id.cardView_Adapter_1);
+        CardView cardView_1 = currentConvertView.findViewById(R.id.cardView_Adapter_1);
 
         cardView_1.setCardBackgroundColor(Color.parseColor(activitiesOfTheDay.get(0).getActivityColor()));
 
         cardView_1.setVisibility(View.VISIBLE);
 
-        TextView textView_1 = convertView.findViewById(R.id.ActivityTV_1);
+        TextView textView_1 = currentConvertView.findViewById(R.id.ActivityTV_1);
 
         textView_1.setText(activitiesOfTheDay.get(0).getActivityName());
 
     }
 
-    private static void setUpSecondEntryOfTheDay(View convertView, ArrayList<TrackedActivity> activitiesOfTheDay) {
+    private static void setUpSecondEntryOfTheDay() {
 
 
-        CardView cardView_2 = convertView.findViewById(R.id.cardView_Adapter_2);
+        CardView cardView_2 = currentConvertView.findViewById(R.id.cardView_Adapter_2);
 
         cardView_2.setCardBackgroundColor(Color.parseColor(activitiesOfTheDay.get(1).getActivityColor()));
 
         cardView_2.setVisibility(View.VISIBLE);
 
-        TextView textView_2 = convertView.findViewById(R.id.ActivityTV_2);
+        TextView textView_2 = currentConvertView.findViewById(R.id.ActivityTV_2);
 
         textView_2.setText(activitiesOfTheDay.get(1).getActivityName());
 
     }
 
-    private static void setUpThirdEntryOfTheDay(View convertView, ArrayList<TrackedActivity> activitiesOfTheDay) {
+    private static void setUpThirdEntryOfTheDay() {
 
-        CardView cardView_3 = convertView.findViewById(R.id.cardView_Adapter_3);
+        CardView cardView_3 = currentConvertView.findViewById(R.id.cardView_Adapter_3);
 
         cardView_3.setCardBackgroundColor(Color.parseColor(activitiesOfTheDay.get(2).getActivityColor()));
 
         cardView_3.setVisibility(View.VISIBLE);
 
-        TextView textView_3 = convertView.findViewById(R.id.ActivityTV_3);
+        TextView textView_3 = currentConvertView.findViewById(R.id.ActivityTV_3);
 
         textView_3.setText(activitiesOfTheDay.get(2).getActivityName());
 
     }
 
-    private static void setUpForthEntryOfTheDay(View convertView, ArrayList<TrackedActivity> activitiesOfTheDay) {
+    private static void setUpForthEntryOfTheDay() {
 
-        CardView cardView_4 = convertView.findViewById(R.id.cardView_Adapter_4);
+        CardView cardView_4 = currentConvertView.findViewById(R.id.cardView_Adapter_4);
 
         cardView_4.setCardBackgroundColor(Color.parseColor(activitiesOfTheDay.get(3).getActivityColor()));
 
         cardView_4.setVisibility(View.VISIBLE);
 
-        TextView textView_4 = convertView.findViewById(R.id.ActivityTV_4);
+        TextView textView_4 = currentConvertView.findViewById(R.id.ActivityTV_4);
 
         textView_4.setText(activitiesOfTheDay.get(3).getActivityName());
 
     }
 
-    private static void setUpMoreThanFourEntryOfTheDay(View convertView) {
+    private static void setUpMoreThanFourEntryOfTheDay() {
 
-        CardView cardView_4 = convertView.findViewById(R.id.cardView_Adapter_4);
+        CardView cardView_4 = currentConvertView.findViewById(R.id.cardView_Adapter_4);
 
         cardView_4.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
 
 
         cardView_4.setVisibility(View.VISIBLE);
 
-        TextView textView_4 = convertView.findViewById(R.id.ActivityTV_4);
+        TextView textView_4 = currentConvertView.findViewById(R.id.ActivityTV_4);
 
         textView_4.setText("  .  .  .  ");
 
         textView_4.setPadding(0, 0, 0, 0);
+
+
+    }
+
+    private static void onClickListenerForFirstActivity(Context context){
+
+        final Context mContext = context;
+
+        TextView textView_entry_1 = currentConvertView.findViewById(R.id.ActivityTV_1);
+
+        final String firstActivity = activitiesOfTheDay.get(0).getActivityName();
+
+        textView_entry_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentToActivityOverview = new Intent(mContext, ActivityOverview.class);
+
+
+                intentToActivityOverview.putExtra("activity", firstActivity);
+
+
+                mContext.startActivity(intentToActivityOverview);
+
+            }
+        });
 
 
     }
